621fabe0f082a09528c425d2b38a00233da2f86f
W. Harrison Wright <wright8191@gmail.com>
2019-02-08 17:55:43 -0600

    Prepare for version 1.0.0

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
 
 setup(
     name='click-command-tree',
-    version='0.1.0',
-    py_modules=['click_command_tree'],
+    version='1.0.0',
+    description='click plugin to show the command tree of your CLI',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/whwright/click-command-tree',
+    author='Harrison Wright',
+    author_email='mail@harrisonwright.me',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    py_modules=['click_command_tree'],\
+    include_package_data=True,
     install_requires=[
         'click',
     ],
